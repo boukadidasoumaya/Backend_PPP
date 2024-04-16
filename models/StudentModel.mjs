41d26cf7d75d061a06a4587ac166bd3778c2edec
boukadida soumaya <soumaya.boukadida@insat.ucar.tn>
2024-04-16 23:10:23 +0100

    filter+ advancemet in  managing errors

--- models/StudentModel.mjs
+++ models/StudentModel.mjs
@@ -18,17 +18,17 @@ const StudentSchema = new mongoose.Schema({
     type: String,
     required: [true, 'CIN is required'],
     unique: true,
-    match: [/^[0-9]{8}$/, 'CIN must be an 8-digit number']
+
   },
   FirstName: {
     type: String,
     required: [true, 'First Name is required'],
-    match: [/^[a-zA-Z]+$/, 'First Name must contain only letters']
+    match: [/^[a-zA-Z \ ]+$/, 'First Name must contain only letters']
   },
   LastName: {
     type: String,
     required: [true, 'Last Name is required'],
-    match: [/^[a-zA-Z]+$/, 'Last Name must contain only letters']
+    match: [/^[a-zA-Z \ ]+$/, 'Last Name must contain only letters']
   },
   Email: {
     type: String,
