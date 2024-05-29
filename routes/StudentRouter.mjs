2b6e76f039007f4a90569e2936cb66481be12d03
boukadida soumaya <soumaya.boukadida@insat.ucar.tn>
2024-05-29 15:38:07 +0100

    some changes

--- routes/StudentRouter.mjs
+++ routes/StudentRouter.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getStudents, getStudentById,getStudentsByMajor, createStudent, updateStudent, deleteStudent, getStudentsByYear, getStudentsByMajorAndByYear,dropStudentsByMajorAndYear} from '../controllers/StudentController.mjs';
+import { getStudents, getStudentById,getStudentsByMajor, createStudent, updateStudent, deleteStudent, getStudentsByYear, getStudentsByMajorAndByYear,dropStudentsByMajorAndYear,getStudentAbsences,getStudentAbsencesBySemester} from '../controllers/StudentController.mjs';
 import { createStudentsByCSV } from '../middleware/ManageCSVFile.mjs';
 const router = express.Router();
 import multer from "multer";
@@ -15,4 +15,6 @@ router.route('/year/:year').get(getStudentsByYear);
 router.route('/majoryear/:major/:year').get(getStudentsByMajorAndByYear);
 router.route('/upload').post(upload.single('csv'),createStudentsByCSV);
 router.route('/drop/:major/:year').delete(dropStudentsByMajorAndYear);
+router.route('/absences/:id').get(getStudentAbsences);
+router.route('/absences/:id/:semester').get(getStudentAbsencesBySemester);
 export default router;
